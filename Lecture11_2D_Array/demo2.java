6d54ce3b4fd04f59d170be3dc662cfc6f1381abe
Rahul Khilari <149717670+Rahul0792@users.noreply.github.com>
2025-03-21 23:51:00 +0530

    2D Array Done successfully

--- Lecture11_2D_Array/demo2.java
+++ Lecture11_2D_Array/demo2.java
@@ -2,7 +2,7 @@ package Lecture11_2D_Array;
 
 import java.util.Scanner;
 
-public class demo1 {
+public class demo2 {
     public static void main(String[] args) {
         int matrix[][] = new int [2][3];
 
@@ -13,13 +13,27 @@ public class demo1 {
             }
         }
 
+        System.out.print("Enter Number want to serch : ");
+        int num = rk.nextInt();
+
         System.out.println("==================================");
         for(int i=0;i<2;i++){
             for(int j=0;j<3;j++){
-                System.out.print (matrix[i][j] + "   " );
+                System.out.print(matrix[i][j] + "   ");
             }
             System.out.println();
         }
+
+
+        for(int i=0;i<2;i++){
+            for(int j=0;j<3;j++){
+                if(num == matrix[i][j]  )
+                System.out.println("index are " + i + " " + j);
+            }
+            System.out.println();
+        }
+
+
     }
 }
 
---
+++ Lecture11_2D_Array/demo3.java
@@ -0,0 +1,7 @@
+package Lecture11_2D_Array;
+
+public class demo3 {
+    public static void main(String[] args) {
+        
+    }
+}
