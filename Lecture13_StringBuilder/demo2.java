677e498d592d7435c509da533aec7a4d7d111b76
Rahul Khilari <149717670+Rahul0792@users.noreply.github.com>
2025-03-24 23:32:37 +0530

    String Builder

---
+++ Lecture13_StringBuilder/demo2.java
@@ -0,0 +1,9 @@
+public class demo2 {
+    public static void main(String[] args) {
+        StringBuilder rk = new StringBuilder("Array");
+        
+        System.out.println(rk.charAt(0));
+        System.out.println(rk.charAt(4));
+
+    }
+}
---
+++ Lecture13_StringBuilder/demo3.java
@@ -0,0 +1,10 @@
+public class demo3 {
+    public static void main(String[] args) {
+        StringBuilder rk = new StringBuilder("Hello");
+
+        System.out.println(rk.insert(0, 'M'));
+
+        // Another way
+        
+    }
+}
