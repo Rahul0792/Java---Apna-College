ca4e009afd760904a668ac52335439b88ca225ff
Rahul Khilari <149717670+Rahul0792@users.noreply.github.com>
2025-03-31 23:23:22 +0530

    Constructor

---
+++ Lecture11_2D_Array/demo11.java
@@ -0,0 +1,11 @@
+package Lecture11_2D_Array;
+
+public class demo11 {
+    public static void main(String[] args) {
+        int marks[] = {10,20,30,40,50};
+        for(int element : marks){
+            System.out.println(element);
+        }
+
+    }
+}
