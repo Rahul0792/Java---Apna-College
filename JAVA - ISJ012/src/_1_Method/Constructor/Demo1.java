208084bb7f681cd106635d89a3caf744d39c9cc9
Rahul Khilari <149717670+Rahul0792@users.noreply.github.com>
2025-04-01 01:35:58 +0530

    Constructor

--- JAVA - ISJ012/src/_1_Method/Constructor/Demo1.java
+++ JAVA - ISJ012/src/_1_Method/Constructor/Demo1.java
@@ -5,8 +5,16 @@ public class Demo1 {
     public static void main(String[] args) {
         Movie movie1 = new Movie();
         Movie movie2 = new Movie("Chhava","Historic Movie",5);
+
+
         System.out.println(movie2.toString());
         System.out.println(movie1.toString());
+
+
+        System.out.println(movie2.Title);
+        System.out.println(movie2.Description);
+        System.out.println(movie2.Rating);
+
     }
 
 }
--- JAVA - ISJ012/src/_1_Method/Constructor/Movie.java
+++ JAVA - ISJ012/src/_1_Method/Constructor/Movie.java
@@ -1,14 +1,25 @@
 package _1_Method.Constructor;
 
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
+
 
 @NoArgsConstructor
-@AllArgsConstructor
 @ToString
-public class Movie {
+@Getter
+@Setter
+@Data
+
+
+public  class Movie {
     String Title;
     String Description;
     int Rating;
+
+
+    public Movie(String title, String description, int rating) {
+        Title = title;
+        Description = description;
+        Rating = rating;
+    }
 }
+
