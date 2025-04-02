bb2e0029cc0c7a027446199df2ba29f069c6cc8c
Rahul Khilari <149717670+Rahul0792@users.noreply.github.com>
2025-04-02 16:37:05 +0530

    array

---
+++ JAVA - ISJ012/src/_10_Array/demo21.java
@@ -0,0 +1,8 @@
+package _10_Array;
+
+public class demo21 {
+    public static void main(String[] args) {
+        int array[]={10,20,30,40};
+        int array2[] = new int[]{10,20,30,4,0,6};
+    }
+}
